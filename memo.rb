45c6735551682934a9aa3b70f8608612e99c3516
EC2 Default User <ec2-user@ip-172-31-0-247.ap-northeast-1.compute.internal>
2022-01-05 22:30:04 +0000

    second commit

--- memo.rb
+++ memo.rb
@@ -6,34 +6,51 @@ require "csv"
     
     puts "あなたは"+memo_type+"を入力しました"
     
-    if memo_type == "1"
+    if memo_type === "1"
         
         puts "拡張子を除いたファイル名を入力してください"
-        memo_name = gets
+        memo_name = gets.chomp
         puts "メモしたい内容を記入してください"
-        memo_content = gets
+        puts "Ctrl + Dで終了してください"
+        memo_contents = readlines(chomp: true)
         memo = CSV.open(memo_name + ".csv","w")
-        memo.puts [memo_content]
+        memo.puts [memo_contents]
         
-    elsif  memo_type == "2"
+    elsif  memo_type === "2"
         
-        puts "拡張子を除いたファイル名を入力してください"
-        memo_name = gets
+        Dir.glob('*?.csv') do |d|
+          puts d
+        end
+        puts "編集するファイル名を拡張子を除いて入力してください"
+        memo_name = gets.chomp
         memo = CSV.read(memo_name+".csv")
         puts "メモの内容は"
         puts "#{memo}です"
-        puts "1(編集する) 2(編集せずに終了する)"
+        puts "1(追記する) 2(削除する) 3(編集せずに終了する)"
         
         edit_type = gets.to_s.chomp
-        if edit_type == "1"
-            puts "編集したい内容を記入してください"
-            memo_content = gets
-            memo = CSV.open(memo_name + ".csv","w")
-            memo.puts [memo_content]
+        if edit_type === "1"
+            puts "追記したい内容を記入してください"
+            memo_contents = readlines(chomp: true)
+            memo = CSV.open(memo_name + ".csv","a")
+            memo.puts [memo_contents]
             puts "編集が完了いたしました"
             
-        elsif edit_type == "2"
+        elsif edit_type === "2"
+            puts "削除したい配列の番号を記入してください"
+            puts "例　[a], [b], [c]"
+            puts "1"
+            puts "[b],[c]"
+            arrangement_type = gets.chomp.to_i
+            memo_contents = CSV.read(memo_name +".csv")
+            memo_contents.delete_at(arrangement_type - 1)
+            memo = CSV.open(memo_name + ".csv","w")
+            memo.puts [memo_contents]
+            p memo
+
+        elsif edit_type === "3"
             puts "操作を終了いたします"
+        
         else
             puts "正しい数字を入力してください"
         end
